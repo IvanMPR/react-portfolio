0adaf1b516ef4a43e1989a601b038ff595369fd7
IvanMPR <ivsanmas@gmail.com>
2024-03-07 17:50:08 +0100

    Update HomeMainSection component

--- src/components/HomeMainSection.jsx
+++ src/components/HomeMainSection.jsx
@@ -3,24 +3,24 @@ import { Link } from "react-router-dom";
 function HomeMainSection() {
   return (
     <>
-      <main className="home container">
-        <div className="home__main-h1">
+      <main className='home container'>
+        <div className='home__main-h1'>
           <h1>
-            <span className="home__main-h1--small wrapper">
+            <span className='home__main-h1--small wrapper'>
               Hello, my name is Ivan Miceta
             </span>
-            <span className="home__main-h1--big wrapper">
+            <span className='home__main-h1--big wrapper'>
               I'm self taught web developer
             </span>
           </h1>
-          <div className="home__main-btn wrapper-l">
-            <Link to="contact" className="btn btn__contact">
+          <div className='home__main-btn wrapper-l'>
+            <Link to='contact' className='btn btn__contact'>
               Contact me
             </Link>
           </div>
         </div>
-        <div className="home__main-image">
-          <img src="/images/blob5test.jpg" alt="test" className="main__blob" />
+        <div className='home__main-image'>
+          <img src='/images/blob5fixed.png' alt='test' className='main__blob' />
         </div>
       </main>
     </>
