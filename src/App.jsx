44a3d12eac4ba4e66ce8ba9391a1551b0543e6f0
IvanMPR <ivsanmas@gmail.com>
2024-03-18 21:00:03 +0100

    Refactor code to use lazy loading and suspense for better performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import { lazy, Suspense } from "react";
 import { HeaderProvider } from "./contexts/HeaderContext.jsx";
 
-import Home from "./pages/Home";
-import Gratitude from "./pages/Gratitude";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Projects from "./pages/Projects";
-import PageNotFound from "./pages/PageNotFound";
-import Header from "./components/header/Header";
+import Spinner from "./components/miscellaneous/Spinner.jsx";
+
+const Home = lazy(() => import("./pages/Home"));
+const Gratitude = lazy(() => import("./pages/Gratitude"));
+const About = lazy(() => import("./pages/About"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Projects = lazy(() => import("./pages/Projects"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const Header = lazy(() => import("./components/header/Header"));
+
+// import Home from "./pages/Home";
+// import Gratitude from "./pages/Gratitude";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import Projects from "./pages/Projects";
+// import PageNotFound from "./pages/PageNotFound";
+// import Header from "./components/header/Header";
 
 function App() {
   return (
     <BrowserRouter basename='/react-portfolio/'>
-      <HeaderProvider>
-        <Header />
-      </HeaderProvider>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='projects' element={<Projects />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='gratitude' element={<Gratitude />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
+      <Suspense fallback={<Spinner />}>
+        <HeaderProvider>
+          <Header />
+        </HeaderProvider>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='projects' element={<Projects />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='gratitude' element={<Gratitude />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
