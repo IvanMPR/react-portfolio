d81a4ae8c91c2d5155a1d465ae20b10b207b0c53
IvanMPR <ivsanmas@gmail.com>
2024-03-18 13:47:49 +0100

    Update image and icon paths

--- src/components/about/AboutBlobImage.jsx
+++ src/components/about/AboutBlobImage.jsx
@@ -1,7 +1,7 @@
 function AboutBlobImage() {
   return (
-    <div className="right-intro__img">
-      <img src="/images/blob5fixed.png" alt="test" className="blob2" />
+    <div className='right-intro__img'>
+      <img src='images/IvanCLP.png' alt='test' className='blob2' />
     </div>
   );
 }
