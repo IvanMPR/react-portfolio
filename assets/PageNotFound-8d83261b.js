eae46270fe098b01272b8c84f38808b824c16d91
IvanMPR <ivsanmas@gmail.com>
2024-03-19 12:35:20 +0100

    Updates

---
+++ assets/PageNotFound-8d83261b.js
@@ -0,0 +1,1 @@
+import{j as o,F as n,i as s}from"./index-1fa920ac.js";function e(){return o.jsxs("div",{className:"not-found",children:[o.jsx("p",{className:"not-found__text",children:"Page Not Found! Use navigation links to go back to existing content !"}),o.jsx("div",{className:"wrapper",children:o.jsx(n,{icon:s,size:"4x",color:"var(--color-primary)"})})]})}export{e as default};
