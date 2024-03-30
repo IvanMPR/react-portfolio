87b4f67e66bd3e025296beb5639c7dbff4b4fbd9
IvanMPR <ivsanmas@gmail.com>
2024-03-30 15:45:28 +0100

    Update CV download link

--- src/components/about/AboutDownloadCVButton.jsx
+++ src/components/about/AboutDownloadCVButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function AboutDownloadCVButton() {
   return (
     <div className='container wrapper btn-wrapper'>
-      <a href='/CV.pdf' download className='btn btn__cv'>
+      <a href='/CV_Ivan_Miceta.pdf' download className='btn btn__cv'>
         Download CV
       </a>
     </div>
